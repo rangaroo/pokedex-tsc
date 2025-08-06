307ebac049865ec6da16c243aa1e348d06319cac
Aslan Onalbek <aslan.onalbek@nu.edu.kz>
2025-08-06 18:05:34 +0500

    Created skeleton of the project

---
+++ src/repl.test.ts
@@ -0,0 +1,19 @@
+import { cleanInput } from "./repl";
+import { describe, expect, test } from "vitest";
+
+describe.each([
+	{
+		input: "  hello  world  ",
+		expected: ["hello", "world"],
+	},
+	// TODO: more test cases here
+])( "cleanInput($input)", ({ input, expected }) => {
+	test(`Expected: ${expected}`, () => {
+		const actual = cleanInput(input);
+		expect(actual).toHaveLength(expected.length);
+
+		for (const i in expected) {
+			expect(actual[i]).toBe(expected[i]);
+		}
+	});
+});
