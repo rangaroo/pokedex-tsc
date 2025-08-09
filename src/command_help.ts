4d9f0dfe960d5ec532164841fa501ea773e86cb2
Aslan Onalbek <aslan.onalbek@nu.edu.kz>
2025-08-09 23:28:50 +0500

    Refactor CLI to use State object for shared state

--- src/command_help.ts
+++ src/command_help.ts
@@ -1,8 +1,10 @@
-import type { CLICommand } from "./command.js";
+import type { State } from "./state.js";
 
-export function commandHelp(commands: Record<string, CLICommand>) {
+export function commandHelp(state: State) {
 	console.log("\nWelcome to the Pokedex!");
 	console.log("Usage: \n");
+
+	const commands = state.commands;
 	for (let command in commands) {
 		console.log(`${commands[command].name}: ${commands[command].description}`);
 	}
